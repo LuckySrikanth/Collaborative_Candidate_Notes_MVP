663ba9e20600ed4e13647df2b64a2eab137fca48
srikanth <luckysrikanth78@gmail.com>
2025-08-31 18:57:35 +0530

    Add Updated ports

--- fronted/src/hooks/useSocket.js
+++ fronted/src/hooks/useSocket.js
@@ -1,7 +1,7 @@
 import { useEffect, useState, useCallback } from "react";
 import io from "socket.io-client";
 
-const SOCKET_SERVER_URL = "http://localhost:4000";
+const SOCKET_SERVER_URL = process.env.SOCKET_SERVER_URL;
 
 let globalSocket = null;
 
